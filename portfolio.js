dee8d6280d0decf780947ad4ab3cefb7a32ad43b
mukhtarani <ma.anileleye@gmail.com>
2022-01-05 01:01:57 +0100

    first

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+function menu(){
+    var hamburger = document.getElementById("hamburger")
+    if(hamburger.style.display==="none"){
+        hamburger.style.display="block";
+    }else{
+       hamburger.style.display="none";
+    }
+}
